d6955c6a66b1dcc1c154cc3db5903d4e7173c7b5
Tyler Hartleb <tyler@hartleb.ca>
2022-11-08 15:55:23 -0700

    Create tab interface
    Created and styled tab interface.

---
+++ Components/TabIcon.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CPSC_481_Digital_Library_Prototype.Components
+{
+    /// <summary>
+    /// Interaction logic for TabIcon.xaml
+    /// </summary>
+    public partial class TabIcon : UserControl
+    {
+        string _TabName = "";
+        public static readonly DependencyProperty _ImagePath = DependencyProperty.Register("bgImage", typeof(ImageSource), typeof(TabIcon), new PropertyMetadata(null));
+
+        public TabIcon()
+        {
+            InitializeComponent();
+        }
+
+        #region Getters and Setters
+
+        public string TabName { get { return _TabName; } set { _TabName = value; } }
+
+        public ImageSource ImagePath { 
+            get { 
+                return (ImageSource)GetValue(_ImagePath); 
+            } 
+            set {
+                Trace.WriteLine(value);
+                SetValue(_ImagePath, value);
+            } 
+        }
+
+        #endregion
+    }
+}
---
+++ Pages/Browse.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC_481_Digital_Library_Prototype.Pages
+{
+    /// <summary>
+    /// Interaction logic for Browse.xaml
+    /// </summary>
+    public partial class Browse : Page
+    {
+        public Browse()
+        {
+            InitializeComponent();
+        }
+    }
+}
