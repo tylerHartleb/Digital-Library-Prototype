a4f7d3f5e6510a1756db3139a34fbf90dbe8ccac
Tyler Hartleb <tyler@hartleb.ca>
2022-11-12 20:36:20 -0700

    Got images working with imports in debug mode

--- Pages/Search.xaml.cs
+++ Pages/Search.xaml.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CPSC_481_Digital_Library_Prototype.Pages
 {
     /// <summary>
     /// Interaction logic for Search.xaml
     /// </summary>
-    public partial class Search : Page
+    public partial class Search : UserControl
     {
         public Search()
         {
