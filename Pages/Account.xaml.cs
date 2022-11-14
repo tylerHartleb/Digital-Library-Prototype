e7c6a90570df25da600d7656512f4535ad869b3d
doowoncho <84430881+doowoncho@users.noreply.github.com>
2022-11-13 23:58:46 -0700

    fixed everythin

---
+++ Pages/Account.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CPSC_481_Digital_Library_Prototype.Pages
+{
+ 
+    public partial class Account : UserControl
+    {
+        public Account()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void CheckOut_Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void OnHold_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Fees_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EmailPassword_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SignOut_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
