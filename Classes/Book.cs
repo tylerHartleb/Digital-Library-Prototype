9dcdfc39adaa913834bde05de09bfc3ff2d0f18a
Tyler Hartleb <tyler@hartleb.ca>
2022-11-25 16:11:22 -0700

    Adjust more stuffs

---
+++ Classes/Book.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPSC_481_Digital_Library_Prototype.Classes
+{
+    public class Book
+    {
+        private string _imagePath;
+        private Dictionary<string, bool> _formatAvailability;
+
+        // Book details
+        private Author _author;
+        private string[] _categories;
+        private string _description = "";
+        private float _rating;
+        private string _title = ""; 
+
+        public Book(Author author, string[] categories, Dictionary<string, bool> formats, string imagePath, float rating)
+        {
+            _author = author;
+            _imagePath = imagePath;
+            _formatAvailability = formats;
+            _rating = rating;
+            _categories = categories;
+        }
+
+        #region Setters
+        public Book SetDescription(string description)
+        {
+            _description = description;
+            return this;
+        }
+
+        public Book SetTitle(string title)
+        {
+            _title = title;
+            return this;
+        }
+        #endregion
+
+        #region Getters
+        public Author GetAuthor()
+        {
+            return _author;
+        }
+        public string GetImagePath()
+        {
+            return _imagePath;
+        }
+
+        public Dictionary<string, bool> GetFormatAvailabilities()
+        {
+            return _formatAvailability;
+        }
+
+        public float GetRating()
+        {
+            return _rating;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+        #endregion
+    }
+}
--- Pages/Search.xaml.cs
+++ Pages/Search.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using CPSC_481_Digital_Library_Prototype.Classes;
+using CPSC_481_Digital_Library_Prototype.Components;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -13,11 +15,22 @@ namespace CPSC_481_Digital_Library_Prototype.Pages
         public Search()
         {
             InitializeComponent();
+            AddRecs();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void AddRecs()
+        {
+            // NOT FINAL THIS IS A TEST
+            foreach(var entry in Books.Instance.GetBooks())
+            {
+                BookDetail bookDetail = new BookDetail(entry.Value);
+                Recs.Children.Add(bookDetail);
+            }
         }
     }
 }
