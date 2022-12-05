4621df8cdd11f484d1be1867c9a881e025e2c21f
FallenByGravity <34290834+FallenByGravity@users.noreply.github.com>
2022-12-04 18:25:47 -0700

    Results and Checks for Login
    The logic for the login and the user class are done. An actual test was not made due to error found when attempted to just test the login page. Should work if said test is done. No change of page is made if the login is done write but comments are but in place to mark all 3 possible outcomes:
    -Username and password are correct
    -Username exists but password wrong
    -Username doesn't exist.

--- Pages/Login.xaml.cs
+++ Pages/Login.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿using CPSC_481_Digital_Library_Prototype.Classes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -8,10 +11,36 @@ namespace CPSC_481_Digital_Library_Prototype.Pages
  
     public partial class Login : UserControl
     {
+        Dictionary<long, string> udb = User.UserDataBase;
+        string enteredId = string.Empty;
+        string enteredPassword = string.Empty;
+        long enteredIdFormated = 0;
         public Login()
         {
             InitializeComponent();
         }
 
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            enteredId = UsernameTextBox.Text;
+            enteredPassword = PasswordTextBox.Text;
+            enteredIdFormated = (long)Convert.ToDouble(enteredPassword);
+            if(udb.ContainsKey(enteredIdFormated) == true)
+            {
+                if(String.Equals(enteredPassword, udb.GetValueOrDefault(enteredIdFormated))){
+                    //This is where we'd send them to the next page since the username exists
+                    //and the password is correct for said username.
+                }
+                else
+                {
+                    //the username exists but the password was wrong.
+                }
+            }
+            else
+            {
+                //The username/id does not exist.
+            }
+            Console.WriteLine("Button clicked");
+        }
     }
 }
